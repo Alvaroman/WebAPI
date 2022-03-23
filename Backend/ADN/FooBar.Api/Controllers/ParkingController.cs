8a41acc680bbc592d4aeb30d3f08b30103d779f3
Álvaro Morales <alvaro.morales@ceiba.com.co>
2022-03-22 22:23:52 -0600

    Greal: Agregando endpoint get de todos los vehículos en el parqueadero.

--- Backend/ADN/FooBar.Api/Controllers/ParkingController.cs
+++ Backend/ADN/FooBar.Api/Controllers/ParkingController.cs
@@ -12,16 +12,21 @@ namespace FooBar.Api.Controllers
         readonly IMediator _mediator = default!;
 
         public ParkingController(IMediator mediator) => _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-
+        
+        [HttpGet]
+        public async Task<IEnumerable<ParkingLotDto>> Get() => await _mediator.Send(new ParkingLotAllQuery());
+        
         [HttpGet("{id}")]
         public async Task<ParkingLotDto> Get(Guid id) => await _mediator.Send(new ParkingLotQuery(id));
 
         [HttpGet("{id}/cost")]
         public async Task<decimal> GetCost(Guid id) => await _mediator.Send(new ParkingLotCostQuery(id));
+
         [HttpPost]
         public async Task Post(ParkingLotCreateCommand parking) => await _mediator.Send(parking);
+
         [HttpPut("{id}/release")]
-        public async Task Post(Guid id) => await _mediator.Send(new ParkingLotReleaseAsyncCommand(id));
+        public async Task Release(Guid id) => await _mediator.Send(new ParkingLotReleaseAsyncCommand(id));
 
     }
 }
