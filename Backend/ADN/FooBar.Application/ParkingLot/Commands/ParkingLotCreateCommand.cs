c22ec1014fbbb5df6adc299afa6e03340577752a
Álvaro Morales <alvaro.morales@ceiba.com.co>
2022-03-24 10:27:34 -0600

    Gral: Editando campos de query.

--- Backend/ADN/FooBar.Application/ParkingLot/Commands/ParkingLotCreateCommand.cs
+++ Backend/ADN/FooBar.Application/ParkingLot/Commands/ParkingLotCreateCommand.cs
@@ -5,7 +5,7 @@ namespace FooBar.Application.ParkingLot.Commands
 {
     public record ParkingLotCreateCommand([Required] int VehicleType,
         [Required] string Plate,
-        [Required] DateTime StartAt,
+        [Required] DateTime StartedAt,
         [Required] int Cylinder
     ) : IRequest;
 }
--- Backend/ADN/FooBar.Application/ParkingLot/Queries/ParkingLotDto.cs
+++ Backend/ADN/FooBar.Application/ParkingLot/Queries/ParkingLotDto.cs
@@ -6,7 +6,7 @@
         public int VehicleType { get; set; } = default!;
         public string Plate { get; set; } = default!;
         public int Cylinder { get; set; }
-        public DateTime StartAt { get; set; } = default!;
+        public DateTime StartedAt { get; set; } = default!;
         public DateTime FinishedAt { get; set; } = default!;
         public bool Status { get; set; } = default!;
     }
