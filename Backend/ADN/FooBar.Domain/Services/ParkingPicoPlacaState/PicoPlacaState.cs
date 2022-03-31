b8bc5ee2b3b5e3023899b0041c07a85fd120a213
Álvaro Morales <alvaro.morales@ceiba.com.co>
2022-03-30 20:56:41 -0600

    Gral: Quitando referencias sin utilizar.

--- Backend/ADN/FooBar.Domain/Services/ParkingPicoPlacaState/PicoPlacaState.cs
+++ Backend/ADN/FooBar.Domain/Services/ParkingPicoPlacaState/PicoPlacaState.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FooBar.Domain.Services.ParkingPicoPlacaState
+﻿namespace FooBar.Domain.Services.ParkingPicoPlacaState
 {
     public abstract class PicoPlacaState
     {
